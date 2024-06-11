f4977d83179f55bf7dee025f17c4ea097846284d
prakashm1988 <prakashm097@gmail.com>
2024-06-11 07:50:25 +0530

    Strings1

---
+++ Ex_070624/Lab_018.py
@@ -0,0 +1,12 @@
+val = None
+#None is a special type of object
+#None is a null value
+#None is a placeholder for an object that does not exist
+#None is a data type
+#None is a keyword
+#None is a constant
+#None is a data type
+#None is a keyword
+print(val)
+
+
---
+++ Ex_070624/Lab_019.py
@@ -0,0 +1,21 @@
+Shopping_list = ["Milk", "Bread", "Butter"]
+print(Shopping_list)
+print(len(Shopping_list))
+print(Shopping_list[0])
+print(Shopping_list[-2])
+
+#my_list = [123, 12.3, True, "Cheese", "Eggs", "Apples", "Bananas"]
+#print(my_list)
+
+Shopping_list.append("Cheese")
+print(Shopping_list)
+Shopping_list.remove("Milk")
+print(Shopping_list)
+Shopping_list.pop()
+print(Shopping_list)
+Shopping_list.append("Jam")
+print(Shopping_list)
+Shopping_list.remove("Jam")
+print(Shopping_list)
+
+
